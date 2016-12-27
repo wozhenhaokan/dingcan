668cd04e7f62eed3acf87ed9d993c391928adfc1
wozhenhaokan <13613266044@163.com>
2016-12-27 15:51:00 +0800

    dingcan

---
+++ db/dao_order.js
@@ -0,0 +1,34 @@
+/**
+ * 操作orders集合的dao模块
+ */
+var connection = require('./connection')
+
+var mongoose = connection.mongoose;
+
+var orderSchema = new mongoose.Schema({
+    "contactor": String,
+    "address": String,
+    "phone": String,
+    "rstName": String,
+    "remark": String,
+    "doorplate": String,
+    "total_money": Number,
+    "peisongfei": Number,
+    "state": Number,
+    "arrive_time": Date,
+    "detail": String,
+    "user_id": String,
+    "coupon_id": String
+});
+
+var OrderModel = mongoose.model('order', orderSchema);
+
+function addOrder(order, callback) {
+    new OrderModel(order).save(callback);
+}
+exports.addOrder = addOrder;
+
+function getOrderById(id, callback) {
+    OrderModel.findOne({_id:id}, callback);
+}
+exports.getOrderById = getOrderById;
